2e8cbb364fd412de100f8a3d86b325e8130b6706
Matt Culbreth <mattc@cygnus.local>
2008-12-05 09:41:29 -0500

    added Staff type

--- freshbooks.rb
+++ freshbooks.rb
@@ -770,7 +770,36 @@ module FreshBooks
       expense_elems.map { |elem| self.new_from_xml(elem) }
     end
   end
-  
+    
+    #--------------------------------------------------------------------------
+    # Staff
+    #==========================================================================
+
+  Staff = BaseObject.new(:staff_id, :username, :first_name, :last_name, 
+    :email,  :business_phone, :mobile_phone, :rate, :last_login,
+    :number_of_logins, :signup_date, 
+    :street1, :street2, :city, :state, :country, :code)
+
+  class Staff
+    TYPE_MAPPINGS = { 'staff_id' => Fixnum }
+
+    def self.get(staff_id)
+      resp = FreshBooks::call_api('staff.get', 'staff_id' => staff_id)
+
+      resp.success? ? self.new_from_xml(resp.elements[1]) : nil
+    end
+
+    def self.list(options = {})
+      resp = FreshBooks::call_api('staff.list', options)
+
+      return nil unless resp.success?
+
+      staff_elems = resp.elements[1].elements
+      staff_elems.map { |elem| self.new_from_xml(elem) }
+    end
+
+  end
+    
 
 end
 
