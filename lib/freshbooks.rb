0daa17f9589dafb1e1cc87896d522ffe257587d3
Ben Curren <ben@outright.com>
2009-03-27 16:11:12 -0700

    Increased the version number.

--- lib/freshbooks.rb
+++ lib/freshbooks.rb
@@ -60,7 +60,7 @@ require 'logger'
 #
 #==============================================================================
 module FreshBooks
-  VERSION = '3.0.1'     # Gem version
+  VERSION = '3.0.2'     # Gem version
   API_VERSION = '2.1' # FreshBooks API version
   SERVICE_URL = "/api/#{API_VERSION}/xml-in"
 
