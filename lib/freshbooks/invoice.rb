c6c0dc1e0ea5b915dda5e8829b52f5147aac6fd3
Ben Curren <ben@outright.com>
2009-08-16 12:09:26 -0700

    Added new fields to invoice.

--- lib/freshbooks/invoice.rb
+++ lib/freshbooks/invoice.rb
@@ -5,10 +5,12 @@ module FreshBooks
       s.fixnum :recurring_id, :read_only => true
       s.float :amount, :amount_outstanding, :discount, :paid
       s.date :date
+      # s.datetime :updated
       s.array :lines
       s.object :links, :read_only => true
       s.string :number, :organization, :status, :notes, :terms, :first_name, :last_name
       s.string :p_street1, :p_street2, :p_city, :p_state, :p_country, :p_code
+      s.string :return_uri
     end
     
     actions :list, :get, :create, :update, :delete, :send_by_email, :send_by_snail_mail
