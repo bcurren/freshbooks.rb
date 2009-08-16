9c999e9e90c81e4b2e7568bec9377bdef2ed418b
Ben Curren <ben@outright.com>
2009-08-16 12:04:18 -0700

    Added vendor to expenses.

--- lib/freshbooks/expense.rb
+++ lib/freshbooks/expense.rb
@@ -4,7 +4,7 @@ module FreshBooks
       s.fixnum :expense_id, :staff_id, :category_id, :project_id, :client_id
       s.float :amount, :tax1_amount, :tax1_percent, :tax2_amount, :tax2_percent
       s.date :date
-      s.string :notes, :status, :tax1_name, :tax2_name
+      s.string :notes, :vendor, :status, :tax1_name, :tax2_name
     end
     
     actions :list, :get, :create, :update, :delete
