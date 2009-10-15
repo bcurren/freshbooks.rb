40ea214e5e07e71864ec19f67de055a54702d087
ivanvr <github@ivanyvenian.com>
2009-10-14 22:05:56 -0500

    Added credit and updated to client. Fixed client.invoices bug.

--- lib/freshbooks/client.rb
+++ lib/freshbooks/client.rb
@@ -6,6 +6,8 @@ module FreshBooks
       s.string :mobile, :fax, :notes, :p_street1, :p_street2, :p_city
       s.string :p_state, :p_country, :p_code, :s_street1, :s_street2
       s.string :s_city, :s_state, :s_country, :s_code
+      s.float :credit
+      s.date_time :updated, :read_only => true
       s.fixnum :client_id
       s.object :links, :read_only => true
     end
@@ -13,7 +15,7 @@ module FreshBooks
     actions :list, :get, :create, :update, :delete
     
     def invoices(options = {})
-      options.merge('client_id' => self.client_id)
+      options.merge!('client_id' => self.client_id)
       Invoice::list(options)
     end
   end
