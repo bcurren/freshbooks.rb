56c0eac5f51041a564dc8f64f28d876870f5a97c
Aaron McGeever <neevor@aaron.(none)>
2010-11-01 12:43:04 -0700

    Bump the version.

--- lib/freshbooks.rb
+++ lib/freshbooks.rb
@@ -61,7 +61,7 @@ require 'logger'
 #
 #==============================================================================
 module FreshBooks
-  VERSION = '3.0.12'     # Gem version
+  VERSION = '3.0.13'     # Gem version
   API_VERSION = '2.1' # FreshBooks API version
   SERVICE_URL = "/api/#{API_VERSION}/xml-in"
 
