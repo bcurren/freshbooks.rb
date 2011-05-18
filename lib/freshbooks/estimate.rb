192a98609890b22c307ba5802a6d43055f8fd326
Matt Dean <matt@trabian.com>
2011-05-18 16:15:43 -0400

    Estimate number is not always a fixed number -- can be a string

--- lib/freshbooks/estimate.rb
+++ lib/freshbooks/estimate.rb
@@ -1,10 +1,10 @@
 module FreshBooks
   class Estimate < FreshBooks::Base
     define_schema do |s|
-      s.string :estimate_id, :status, :date, :notes, :terms, :first_name
+      s.string :estimate_id, :status, :date, :notes, :terms, :first_name, :number
       s.string :last_name, :organization, :p_street1, :p_street2, :p_city
       s.string :p_state, :p_country, :p_code
-      s.fixnum :client_id, :po_number, :number
+      s.fixnum :client_id, :po_number
       s.float :discount, :amount
       s.array :lines
       s.object :links, :read_only => true
