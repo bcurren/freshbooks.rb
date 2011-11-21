84768dcfd07537132701a6af4a0ac4811165fdd8
Andrew Kalek <andrew.kalek@anlek.com>
2011-11-20 16:46:57 -0800

    Updated client API

--- lib/freshbooks/links.rb
+++ lib/freshbooks/links.rb
@@ -1,7 +1,7 @@
 module FreshBooks
   class Links < FreshBooks::Base
     define_schema do |s|
-      s.string :client_view, :view, :edit, :read_only => true
+      s.string :client_view, :view, :edit, :statement, :read_only => true
     end
   end
 end
